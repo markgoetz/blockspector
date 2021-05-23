fcfe4089dbc3ddcbb43c9f536335909a488ef59e
Mark Goetz <breakawayblue@gmail.com>
2021-05-23 08:57:27 -0400

    Highlight Spacer on mouseover

--- src/components/canvas/Spacer.tsx
+++ src/components/canvas/Spacer.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
-import { COLORS } from '../../styles/variables';
+import React, { useState } from 'react';
 import { Vector3 } from 'three';
+import { ThreeEvent } from '@react-three/fiber';
+import { COLORS } from '../../styles/variables';
+import { DELETE_ID } from '../../constants/blocks';
+import { useSelectedBlockId } from '../../context/ToolbarContext';
 
 type Props = {
     position: Vector3;
 };
 
 const Spacer: React.FC<Props> = ({ position }) => {
+    const [isHighlighted, setIsHighlighted] = useState(false);
+    const selectedId = useSelectedBlockId();
+
+    const onPointerOver = (e: ThreeEvent<PointerEvent>) => {
+        if (selectedId === DELETE_ID) {
+            setIsHighlighted(true);
+        } else {
+            // highlight the face
+        }
+    };
+
+    const onPointerLeave = () => {
+        setIsHighlighted(false);
+    };
+
+    const color = isHighlighted ? COLORS.SECONDARY : COLORS.PRIMARY.BRIGHT;
+
+
     return (
-        <mesh visible position={[position.x, position.y, position.z]}>
+        <mesh
+            visible
+            position={[position.x, position.y, position.z]}
+            onPointerOver={onPointerOver}
+            onPointerLeave={onPointerLeave}
+        >
             <boxGeometry args={[1, 1, 1]} />
             <meshBasicMaterial
                 attach="material"
-                color={COLORS.PRIMARY.BRIGHT}
+                color={color}
                 wireframe={true}
             />
         </mesh>
