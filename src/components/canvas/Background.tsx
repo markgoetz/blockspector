bb9ac316f65ef291bffbe1269583af7118e4c4ec
Mark Goetz <breakawayblue@gmail.com>
2021-06-01 16:51:14 -0500

    Fix formatting

--- src/components/canvas/Background.tsx
+++ src/components/canvas/Background.tsx
@@ -7,10 +7,7 @@ type Props = {
 
 const Background: React.FC<Props> = ({ y }) => {
     return (
-        <mesh
-            visible
-            position={[0, y, 0]}
-        >
+        <mesh visible position={[0, y, 0]}>
             <boxGeometry args={[2000, 1, 2000]} />
             <meshBasicMaterial attach="material" color={COLORS.GRAY.LIGHT} />
         </mesh>
