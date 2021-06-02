3044f4f62fe8439d94d6974f6f055456e76f6e29
Mark Goetz <breakawayblue@gmail.com>
2021-06-02 11:26:52 -0500

    Refactor BuildState into its own component

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,8 +1,6 @@
 /** @jsx jsx */
 import { Global, jsx } from '@emotion/react';
-import React, { Suspense, useState } from 'react';
-import { Vector3 } from 'three';
-import { ThreeEvent } from '@react-three/fiber';
+import React, { Suspense } from 'react';
 
 import BlockCanvas from './canvas/BlockCanvas';
 import Footer from './ui/Footer';
@@ -13,81 +11,35 @@ import UIGrid from './ui/UIGrid';
 import FONT_FACE from '../styles/fonts';
 import GLOBAL_STYLES from '../styles/global';
 import RESET from '../styles/reset';
-import { useSelectedBlockId } from '../context/ToolbarContext';
-import PositionedBlock from '../definitions/PositionedBlock';
-import { DELETE_ID } from '../constants/blocks';
-import createBlock from '../lib/createBlock';
+import BuildState from './BuildState';
 
 const AppLayout: React.FC = () => {
-    const selectedBlockId = useSelectedBlockId();
-    const [blocks, setBlocks] = useState<PositionedBlock[]>([]);
-
-    const isDeleteSelected = selectedBlockId === DELETE_ID;
-
-    const onCanvasClick = () => {
-        if (!isDeleteSelected && selectedBlockId != null && blocks.length === 0) {
-            const block = createBlock(
-                selectedBlockId,
-                new Vector3(0, 0, 0),
-            );
-            setBlocks([...blocks, block]);
-        }
-    };
-
-    const onBlockClick = (
-        e: ThreeEvent<MouseEvent>,
-        block: PositionedBlock,
-    ) => {
-        e.stopPropagation();
-
-        if (selectedBlockId == null) {
-            return;
-        }
-
-        if (e.face == null) {
-            return;
-        }
-
-        if (isDeleteSelected) {
-            setBlocks(blocks.filter((b) => b.uuid !== block.uuid));
-            return;
-        }
-
-        const objectPosition = new Vector3(
-            e.object.position.x,
-            e.object.position.y,
-            e.object.position.z,
-        );
-        const newPosition = objectPosition.add(e.face.normal);
-
-        if (blocks.some((block) => block.position === newPosition)) {
-            return;
-        }
-
-        const newBlock = createBlock(selectedBlockId as string, newPosition);
-        setBlocks([...blocks, newBlock]);
-    };
+    
 
     return (
         <React.Fragment>
             <Global styles={RESET} />
             <Global styles={FONT_FACE} />
             <Global styles={GLOBAL_STYLES} />
-            <UIGrid
-                header={<Header />}
-                sidebar={<Sidebar blocks={blocks} />}
-                toolbar={<Toolbar />}
-                footer={<Footer />}
-                canvas={
-                    <Suspense fallback={null}>
-                        <BlockCanvas
-                            onBlockClick={onBlockClick}
-                            onCanvasClick={onCanvasClick}
-                            blocks={blocks}
-                        />
-                    </Suspense>
-                }
-            />
+            <BuildState>
+                {(blocks, onCanvasClick, onBlockClick) => (
+                    <UIGrid
+                        header={<Header />}
+                        sidebar={<Sidebar blocks={blocks} />}
+                        toolbar={<Toolbar />}
+                        footer={<Footer />}
+                        canvas={
+                            <Suspense fallback={null}>
+                                <BlockCanvas
+                                    onBlockClick={onBlockClick}
+                                    onCanvasClick={onCanvasClick}
+                                    blocks={blocks}
+                                />
+                            </Suspense>
+                        }
+                    />
+                )}
+            </BuildState>
         </React.Fragment>
     );
 };
---
+++ src/components/BuildState.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Vector3 } from 'three';
+import { ThreeEvent } from '@react-three/fiber';
+
+import { useSelectedBlockId } from '../context/ToolbarContext';
+import PositionedBlock from '../definitions/PositionedBlock';
+import { DELETE_ID } from '../constants/blocks';
+import createBlock from '../lib/createBlock';
+
+type Props = {
+    children: (
+        blocks: PositionedBlock[],
+        onCanvasClick: () => void,
+        onBlockClick: (e: ThreeEvent<MouseEvent>, block: PositionedBlock) => void,
+    ) => JSX.Element,
+}
+
+const BuildState: React.FC<Props> = (props) => {
+    const selectedBlockId = useSelectedBlockId();
+    const [blocks, setBlocks] = useState<PositionedBlock[]>([]);
+
+    const isDeleteSelected = selectedBlockId === DELETE_ID;
+
+    const onCanvasClick = () => {
+        if (!isDeleteSelected && selectedBlockId != null && blocks.length === 0) {
+            const block = createBlock(
+                selectedBlockId,
+                new Vector3(0, 0, 0),
+            );
+            setBlocks([...blocks, block]);
+        }
+    };
+
+    const onBlockClick = (
+        e: ThreeEvent<MouseEvent>,
+        block: PositionedBlock,
+    ) => {
+        e.stopPropagation();
+
+        if (selectedBlockId == null) {
+            return;
+        }
+
+        if (e.face == null) {
+            return;
+        }
+
+        if (isDeleteSelected) {
+            setBlocks(blocks.filter((b) => b.uuid !== block.uuid));
+            return;
+        }
+
+        const objectPosition = new Vector3(
+            e.object.position.x,
+            e.object.position.y,
+            e.object.position.z,
+        );
+        const newPosition = objectPosition.add(e.face.normal);
+
+        if (blocks.some((block) => block.position === newPosition)) {
+            return;
+        }
+
+        const newBlock = createBlock(selectedBlockId as string, newPosition);
+        setBlocks([...blocks, newBlock]);
+    };
+
+    return props.children(blocks, onCanvasClick, onBlockClick);
+};
+
+export default BuildState;
