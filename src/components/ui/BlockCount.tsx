080a3efdd3594d42cf9d437cda57c6e17bc70fe5
Mark Goetz <breakawayblue@gmail.com>
2021-06-02 14:10:58 -0500

    Style block count module

--- src/components/ui/BlockCount.tsx
+++ src/components/ui/BlockCount.tsx
@@ -1,14 +1,34 @@
 /** @jsx jsx */
-import { jsx } from '@emotion/react';
+import { jsx, CSSObject } from '@emotion/react';
 import React from 'react';
+import { SIZES } from '../../styles/variables';
 import PositionedBlock from '../../definitions/PositionedBlock';
+import BLOCKS from '../../constants/blocks';
 
 type Props = {
     blocks: PositionedBlock[];
 };
 
-const BlockCount: React.FC<Props> = ({ blocks }) => {
-    const blocksByType = blocks.reduce((previous, block) => {
+const TABLE_STYLE: CSSObject = {
+    width: '100%',
+};
+
+const CELL_STYLE: CSSObject = {
+    verticalAlign: 'middle',
+};
+
+const COUNT_CELL_STYLE: CSSObject = {
+    ...CELL_STYLE,
+    textAlign: 'right',
+};
+
+const IMAGE_STYLE: CSSObject = {
+    width: SIZES.DOUBLE,
+    height: SIZES.DOUBLE,
+};
+
+const getBlocksByType = (blocks: PositionedBlock[]) => {
+    return blocks.reduce((previous, block) => {
         if (previous[block.blockId] == null) {
             return {
                 ...previous,
@@ -21,10 +41,35 @@ const BlockCount: React.FC<Props> = ({ blocks }) => {
             [block.blockId]: previous[block.blockId] + 1,
         };
     }, {} as Record<string, number>);
+};
 
-    console.log(blocksByType);
+const BlockCount: React.FC<Props> = ({ blocks }) => {
+    const blocksByType = getBlocksByType(blocks);
 
-    return <div>block count</div>;
+    return (
+        <table css={TABLE_STYLE} cellSpacing={0} cellPadding={0}>
+            {Object.keys(blocksByType).map(blockId => {
+                const count = blocksByType[blockId]
+                const block = BLOCKS.find(block => block.id === blockId );
+                if (block == null) {
+                    throw new Error(`Unknown block ID ${blockId}`);
+                }
+            
+                return (
+                    <tr>
+                        <td css={CELL_STYLE}>
+                            <img
+                                src={`/assets/textures/${block.imageUrl}`}
+                                css={IMAGE_STYLE}
+                            />
+                        </td>
+                        <td css={CELL_STYLE}>{block.name}</td>
+                        <td css={COUNT_CELL_STYLE}>x {count}</td>
+                    </tr>
+                );
+            })}
+        </table>
+    )
 };
 
 export default BlockCount;
